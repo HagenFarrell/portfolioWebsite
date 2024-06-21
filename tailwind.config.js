af66e1e9f1d24b41cbfe2a829004eb557b9ec168
HagenFarrell <hagen.farrell@gmail.com>
2024-06-21 19:47:14 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Add paths to all of your template files
+    './index.html',          // Ensure index.html is included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
